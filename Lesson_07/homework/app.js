1a00586a4d9aea86c2e7b8358703091519b6d761
sSwiergosz <szymon.swiergosz@gmail.com>
2018-01-12 22:16:59 +0100

    Homework ready

--- Lesson_07/homework/app.js
+++ Lesson_07/homework/app.js
@@ -1,11 +1,11 @@
 function main() {
   getLocations();
-  // console.log(arrayOfLocations);
   if (Modernizr.geolocation) {
     navigator.geolocation.getCurrentPosition(success, fail);
   }
 }
 
+// read coordinates and names from JSON file
 function getLocations() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'map_service.json', true);
@@ -19,62 +19,59 @@ function getLocations() {
               responseObject.features[i].geometry.coordinates[1],
               responseObject.features[i].geometry.coordinates[0],
             ]
-          )
+          );
+          namesOfLocations.push(responseObject.features[i].properties.long_name);
         }
-        console.log(arrayOfLocations);
     }
   };
 
   xhr.send(null);
 }
 
+// put marker on a map based on your current location
 function success(location) {
   let vector = [];
   document.getElementsByTagName('div')[0].id = 'mapid';
   vector[0] = location.coords.latitude;
   vector[1] = location.coords.longitude;
-  // arrayOfLocations.push(vector);
-  // draw(vector);
+  draw(vector);
 }
 
 function fail(msg) {
   console.log(`Error code: ${msg.code}`);
 }
 
-// function draw(vector) {
-//   const mymap = L.map('mapid').setView(vector, 13);
-//
-//   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-//     maxZoom: 18,
-//     id: 'mapbox.streets'
-//   }).addTo(mymap);
-//
-//   let marker = L.marker(vector).addTo(mymap);
-//   marker.bindPopup("Punkt o wspołrzędnych (" + vector[0] + ", " + vector[1] + ")").openPopup();
-//
-//   var circle = L.circle(vector, {
-//     color: 'green',
-//     fillColor: '#00AA00',
-//     fillOpacity: 0.5,
-//     radius: 1000
-//   }).addTo(mymap);
-//
-//   function onMapClickMarker(e) {
-//     arrayOfLocations.push([e.latlng.lat, e.latlng.lng]);
-//     console.log(arrayOfLocations);
-//     let marker2 = L.marker();
-//     marker2.setLatLng(e.latlng)
-//       .addTo(mymap);
-//     marker2.bindPopup("Kliknąłeś na punkt o wspołrzędnych " + e.latlng.toString().replace('LatLng', '')).openPopup();
-//     let polyline = L.polyline(arrayOfLocations, {color: 'red'}).addTo(mymap);
-//   }
-//
-//   mymap.on('click', onMapClickMarker);
-//
-//   L.control.scale({imperial: false}).addTo(mymap);
-//
-//   let polyline = L.polyline(arrayOfLocations, {color: 'red'}).addTo(mymap);
-// }
+function draw(vector) {
+  const mymap = L.map('mapid').setView(vector, 13);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    id: 'mapbox.streets'
+  }).addTo(mymap);
+
+  const red_marker = new L.Icon({
+  	iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  });
+
+  let marker = L.marker(vector, {icon: red_marker}).addTo(mymap);
+
+  for(let i = 0; i < arrayOfLocations.length; i++) {
+  	let vector = [arrayOfLocations[i][0], arrayOfLocations[i][1]];
+  	L.marker(vector).addTo(mymap).bindPopup(namesOfLocations[i]);
+
+  	let circle = L.circle(vector, {
+	    color: 'blue',
+	    fillColor: 'lightblue',
+	    fillOpacity: 0.5,
+	    radius: 30
+	  }).addTo(mymap);
+  }
+
+  // bind popup to your current location marker
+  marker.bindPopup("Tu jesteś").openPopup();
+  L.control.scale({imperial: false}).addTo(mymap);
+}
 
 let arrayOfLocations = [];
+let namesOfLocations = [];
 window.onload = main;
